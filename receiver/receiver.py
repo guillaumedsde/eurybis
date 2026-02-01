aa15b6a80908374edabde7cde0d4fb3d957ca3a1
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2026-02-01 11:22:14 +0100

    feat: initial lidi compose and basic receiver

---
+++ receiver/receiver.py
@@ -0,0 +1,31 @@
+import asyncio
+
+
+async def handle_echo(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info("peername")
+
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the connection")
+    writer.close()
+    await writer.wait_closed()
+
+
+async def main():
+    server = await asyncio.start_server(handle_echo, "127.0.0.1", 6002)
+
+    addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
+    print(f"Serving on {addrs}")
+
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
