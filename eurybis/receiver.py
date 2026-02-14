f7b6685ad72c983f4925f8a42695f09cbde3cd6f
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2026-02-14 12:20:55 +0100

    feat(receiver): async write to file

--- eurybis/receiver.py
+++ eurybis/receiver.py
@@ -1,20 +1,20 @@
 import argparse
 import asyncio
 import logging
+import pathlib
+import uuid
 
 LOGGER = logging.getLogger(__name__)
 
 
-async def handle_echo(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
+async def handle_file(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     addr = writer.get_extra_info("peername")
-
-    LOGGER.info(f"Received {message!r} from {addr!r}")
-
-    LOGGER.info(f"Send: {message!r}")
-    writer.write(data)
-    await writer.drain()
+    LOGGER.info(f"Received from {addr!r}")
+    filepath = DATA_DIRECTORY / str(uuid.uuid4())
+    with filepath.open("wb") as dest_file:
+        while chunk := await reader.read(CHUNK_SIZE):
+            # Offload file write to thread to avoid blocking
+            await asyncio.to_thread(dest_file.write, chunk)
 
     LOGGER.info("Close the connection")
     writer.close()
@@ -22,7 +22,7 @@ async def handle_echo(reader, writer):
 
 
 async def receiver_server(address: str, port: int):
-    server = await asyncio.start_server(handle_echo, address, port)
+    server = await asyncio.start_server(handle_file, address, port)
 
     addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
     LOGGER.info(f"Serving on {addrs}")
@@ -35,6 +35,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="eurybis-receive",
         description="Receiver for Eurybis, receives data from lidi-send and stores it on disk",
+    )
+    parser.add_argument(
+        "data_directory",
+        type=pathlib.Path,
     )
     parser.add_argument(
         "--listen-address",
@@ -52,9 +56,22 @@ if __name__ == "__main__":
         default=logging.getLevelName(logging.INFO),
         choices=logging.getLevelNamesMapping().keys(),
     )
+    parser.add_argument(
+        "--chunk-size",
+        type=int,
+        default=2 * 1024 ^ 2,  # 2MB,
+    )
     args = parser.parse_args()
+
+    DATA_DIRECTORY: pathlib.Path = args.data_directory
+    CHUNK_SIZE: int = args.chunk_size
 
     logging.basicConfig(
         level=logging.getLevelNamesMapping()[args.log_level],
     )
-    asyncio.run(receiver_server(args.listen_address, args.listen_port))
+    asyncio.run(
+        receiver_server(
+            args.listen_address,
+            args.listen_port,
+        )
+    )
